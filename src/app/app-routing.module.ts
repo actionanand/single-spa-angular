dce3ac7abe139f009e213fbd9d72d8fc04931414
Anand raja <meetanandns@gmail.com>
2022-07-27 17:18:55 +0000

    single spa angular

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+
+const routes: Routes = [
+  { path: '**', component: EmptyRouteComponent, title: 'Single-Spa Angular' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }],
+})
+export class AppRoutingModule { }
