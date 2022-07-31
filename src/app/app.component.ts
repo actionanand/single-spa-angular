65473761f2d482d7aafc5470d1ab17fe63c56cec
Anand raja <meetanandns@gmail.com>
2022-07-31 18:31:29 +0000

    props added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { singleSpaPropsSubject, SingleSpaProps } from 'src/single-spa/single-spa-props';
+import { Subscription } from 'rxjs';
+import { debounceTime, take } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'single-spa-angular';
+
+  singleSpaProps!: SingleSpaProps;
+  subscription!: Subscription;
+
+  ngOnInit(): void {
+    this.subscription = singleSpaPropsSubject.pipe(debounceTime(0), take(1)).subscribe( props => {
+      this.singleSpaProps = props;
+      console.log(props);
+    });
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  // OR if you don't need to access `singleSpaProps` inside the component
+  // then create `Observable` property and use it in template with `async` pipe.
+  singleSpaProps$ = singleSpaPropsSubject.asObservable();
 }
