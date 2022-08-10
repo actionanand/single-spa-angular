61a5eab443e53c16228d91188f68622addf018d9
Anand raja <meetanandns@gmail.com>
2022-08-10 16:55:47 +0000

    new todo

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { singleSpaPropsSubject, SingleSpaProps } from 'src/single-spa/single-spa-props';
 import { Subscription } from 'rxjs';
 import { debounceTime, take } from 'rxjs/operators';
 
+import { getData, state$ } from '@actionanand/utility';
+
+import { singleSpaPropsSubject } from 'src/single-spa/single-spa-props';
 import { TodoService } from '../services/todo.service';
 
 @Component({
@@ -20,6 +22,7 @@ export class TodoComponent implements OnInit, OnDestroy {
 
   routerSub!: Subscription;
   singleSpaProbSub!: Subscription;
+  utiSub!: Subscription;
 
   title = 'Angular Todo';
 
@@ -33,6 +36,19 @@ export class TodoComponent implements OnInit, OnDestroy {
 
     this.singleSpaProbSub = singleSpaPropsSubject.pipe(debounceTime(0), take(1)).subscribe((props: any) => {
       this.title = props.angTitle;
+    });
+
+    getData('/data').then((data: any) => {
+      console.log('angular ', data);
+    });
+
+    this.utiSub = state$.subscribe((resp: any) => {
+      // console.log('angular ', resp.data.angTodo);
+      const newTodoTxt = resp.data?.angTodo;
+      if (newTodoTxt) {
+        this.newTodo = newTodoTxt;
+        this.addTodo();
+      }
     });
   }
 
@@ -80,5 +96,6 @@ export class TodoComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.routerSub.unsubscribe();
     this.singleSpaProbSub.unsubscribe();
+    this.utiSub.unsubscribe();
   }
 }
